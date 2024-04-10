22a34dbd2b9633dfd75ffb572ea924da46d14925
eliasinacio <0.elias.inacio@gmail.com>
2024-04-10 10:47:22 -0300

    renaming and creating new columns

--- main.py
+++ main.py
@@ -2,6 +2,26 @@
 
 import pandas as pd
 
-df_main = pd.read_excel("./tabela-de-acoes.xlsx", sheet_name="Principal")
+df_main = pd.read_excel("./tabela-de-acoes.xlsx", sheet_name="main")
 
-print(df_main.head(10))
+# print(df_main)
+# print()
+
+df_main = df_main[['Ativo', 'Data', 'Último (R$)', 'Var. Dia (%)']].copy()
+
+# print(df_main.head(20))
+# print()
+
+df_main = df_main.rename(columns={'Ativo': 'ticker', 'Data': 'date', 'Último (R$)': 'day_last_price_brl', 'Var. Dia (%)': 'day_variation_pct'}).copy()
+
+# print(list(df_main.columns.values))
+# print()
+
+# print(df_main.head(20))
+# print()
+
+# Creating new columns
+df_main['variation_pct'] = df_main['day_variation_pct'] / 100
+df_main['day_first_price_brl'] = df_main['day_last_price_brl'] / ( 1 + df_main['variation_pct'])
+
+print(df_main)
