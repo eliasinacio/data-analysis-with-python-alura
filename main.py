ac2ceb7a0ee5eaf88702233fd1825c812b544700
eliasinacio <0.elias.inacio@gmail.com>
2024-04-11 10:54:32 -0300

    merging tables, creating new columns and formating data

--- main.py
+++ main.py
@@ -2,26 +2,43 @@
 
 import pandas as pd
 
-df_main = pd.read_excel("./tabela-de-acoes.xlsx", sheet_name="main")
+# Changes all float cells to a given format
+pd.options.display.float_format = '{:.2f}'.format
 
-# print(df_main)
-# print()
 
+# Gettting the Main spreadsheet
+df_main = pd.read_excel("./tabela_de_acoes.xlsx", sheet_name="main")
+
+
+# Getting specific columns
 df_main = df_main[['Ativo', 'Data', 'Último (R$)', 'Var. Dia (%)']].copy()
 
-# print(df_main.head(20))
-# print()
 
+# Renaming columns
 df_main = df_main.rename(columns={'Ativo': 'ticker', 'Data': 'date', 'Último (R$)': 'day_last_price_brl', 'Var. Dia (%)': 'day_variation_pct'}).copy()
 
-# print(list(df_main.columns.values))
-# print()
-
-# print(df_main.head(20))
-# print()
 
 # Creating new columns
 df_main['variation_pct'] = df_main['day_variation_pct'] / 100
 df_main['day_first_price_brl'] = df_main['day_last_price_brl'] / ( 1 + df_main['variation_pct'])
 
-print(df_main)
+print()
+print(df_main.head(10))
+
+
+# Getting the all_stocks page and merging it into df_main
+df_all_stocks = pd.read_excel("./tabela_de_acoes.xlsx", sheet_name="all_stocks")
+df_all_stocks = df_all_stocks.rename(columns={'Código': 'ticker', 'Qtde. Teórica': 'total_stocks'})
+
+df_main = df_main.merge(df_all_stocks, left_on='ticker', right_on='ticker', how='left')
+
+df_main['total_stocks'] = df_main['total_stocks'].astype(int)
+
+print()
+print(df_main.head(10))
+
+
+df_main['total_variation_brl'] = (df_main['day_last_price_brl'] - df_main['day_first_price_brl']) * df_main['total_stocks']
+
+print()
+print(df_main.head(10))
